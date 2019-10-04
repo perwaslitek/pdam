3e901ed142774702beffcedf9bc3f565ea2cbc54
perwaslitek <54173027+perwaslitek@users.noreply.github.com>
2019-10-04 16:37:37 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://github.com/perwaslitek/pdam';
+$config['base_url'] = 'https://perwaslitek.github.io/pdam/';
 
 /*
 |--------------------------------------------------------------------------
